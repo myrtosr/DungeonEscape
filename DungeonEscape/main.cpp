8058a21a65d0ca1c0fea89b6344d6420e53aba62
pelxts <tsoumenipelagia@gmail.com>
2026-01-03 19:14:22 +0200

    changed

--- DungeonEscape/main.cpp
+++ DungeonEscape/main.cpp
@@ -1,6 +1,26 @@
-
 #include "sgg/graphics.h"
 
+void update(float ms) {
+
+}
+
+void draw() {
+
+}
+
 int main() {
+
+    graphics::createWindow(1366, 768, "Tutorial");
+
+
+    graphics::setDrawFunction(draw);
+    graphics::setUpdateFunction(update);
+
+    graphics::setCanvasSize(1366, 768);
+    graphics::setCanvasScaleMode(graphics::CANVAS_SCALE_FIT);
+
+   
+    graphics::startMessageLoop();
+ 
 	return 0;
 }
