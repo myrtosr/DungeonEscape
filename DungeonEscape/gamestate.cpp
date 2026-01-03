3e2128cd4ad8d0e527c7e39efb0f9484e64ab1d4
pelxts <tsoumenipelagia@gmail.com>
2026-01-03 23:25:39 +0200

    changes

--- DungeonEscape/gamestate.cpp
+++ DungeonEscape/gamestate.cpp
@@ -1,15 +1,89 @@
+#include "sgg/graphics.h"
 #include "gamestate.h"
+#include "config.h"
+
+void GameState::updateStartScreen()
+{
+	graphics::MouseState ms;
+	graphics::getMouseState(ms);
+	if (ms.button_left_pressed)
+		status = STATUS_PLAYING;
+}
+
+void GameState::updateLevelScreen()
+{
+}
+
+void GameState::updateEndScreen()
+{
+}
+
+void GameState::drawStartScreen()
+{
+	graphics::setFont(std::string(ASSET_PATH) + "title_font.ttf");
+
+	graphics::Brush br;
+	br.texture = std::string(ASSET_PATH) + "menu_image.png";
+	br.outline_opacity = 0.0f;
+	graphics::drawRect(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2, CANVAS_WIDTH, CANVAS_WIDTH, br);
+
+	br.texture = "";
+	char info[40];
+	sprintf_s(info, "Start Game");
+	graphics::drawText(CANVAS_WIDTH * 0.455, CANVAS_HEIGHT * 0.55, 30, info, br);
+
+	graphics::MouseState ms;
+	graphics::getMouseState(ms);
+	br.texture = std::string(ASSET_PATH) + "cursor.png";
+	graphics::drawRect(window2canvasX(ms.cur_pos_x), window2canvasY(ms.cur_pos_y), 30, 30, br);
+}
+
+void GameState::drawLevelScreen()
+{
+}
+
+void GameState::drawEndScreen()
+{
+}
 
 void GameState::update()
 {
+	if (status == STATUS_START) {
+		updateStartScreen();
+	}
+	else if (status == STATUS_PLAYING) {
+		updateLevelScreen();
+	}
+	else if (status == STATUS_END) {
+		updateEndScreen();
+	}
 }
 
 void GameState::draw()
 {
+	if (status == STATUS_START) {
+		drawStartScreen();
+	}
+	else if (status == STATUS_PLAYING) {
+		drawLevelScreen();
+	}
+	else if (status == STATUS_END) {
+		drawEndScreen();
+	}
 }
 
 void GameState::init()
 {
+}
+
+float GameState::window2canvasX(float x)
+{
+	return x * CANVAS_WIDTH / (float)window_width;
+}
+
+float GameState::window2canvasY(float y)
+{
+	return y * CANVAS_HEIGHT / (float)window_height;
 }
 
 GameState::GameState()
--- DungeonEscape/gamestate.h
+++ DungeonEscape/gamestate.h
@@ -1,6 +1,11 @@
 #pragma once
+#include "config.h"
 
 class GameState {
+	// game states -----------
+	typedef enum { STATUS_START, STATUS_PLAYING, STATUS_END } status_t;
+	status_t status = STATUS_START;
+	//------------------------
 	// update functions for different states of the game
 	void updateStartScreen();
 	void updateLevelScreen();
@@ -9,10 +14,19 @@ class GameState {
 	void drawStartScreen();
 	void drawLevelScreen();
 	void drawEndScreen();
+	//------------------------
+	unsigned int window_width = WINDOW_WIDTH;
+	unsigned int window_height = WINDOW_HEIGHT;
 public:
 	void update();
 	void draw();
 	void init();
+	// Mouse handling
+	unsigned int getWindowWidth() { return window_width; };
+	unsigned int getWindowHeight() { return window_height; };
+	float window2canvasX(float x);
+	float window2canvasY(float y);
+	void setWindowDimensions(unsigned int w, unsigned int h) { window_width = w; window_height = h; }
 	// Constructor & Destructor 
 	GameState();
 	~GameState();
