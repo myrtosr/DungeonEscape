17a356e71fc4aa1674bca7f2e5940dcbf2f25bc1
myrtosr <myrsyrigou@gmail.com>
2026-01-04 14:44:22 +0200

    <3

---
+++ DungeonEscape/gameobject.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+// class Collidable {}
+
+class GameObject {
+protected:
+	const class Game& game;
+public:
+	GameObject(const class Game& mygame);
+	virtual void update() = 0;
+	virtual void draw() = 0;
+	virtual void init() = 0;
+};
