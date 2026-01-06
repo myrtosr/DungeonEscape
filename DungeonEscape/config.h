e62f6eea800108ebce264c293407b2c13ad10f7c
pelxts <tsoumenipelagia@gmail.com>
2026-01-06 10:12:08 +0200

    changes

--- DungeonEscape/config.h
+++ DungeonEscape/config.h
@@ -6,7 +6,7 @@
 // defining the canvas size
 #define CANVAS_WIDTH 1680
 #define CANVAS_HEIGHT 960
-// Defining the grid size
+// Defining the grid map size
 #define TILE_SIZE 48
 #define TILES_X (CANVAS_WIDTH / TILE_SIZE)
 #define TILES_Y (CANVAS_HEIGHT / TILE_SIZE)
